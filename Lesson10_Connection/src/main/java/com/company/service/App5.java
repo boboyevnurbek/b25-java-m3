d7695a52046f02f914a3f3deb2fdec438c432715
boboyevnurbek <boboyevnurbek95@gmail.com>
2022-11-18 18:36:19 +0500

    5-kommit in lesson 10

---
+++ Lesson10_Connection/src/main/java/com/company/service/App5.java
@@ -0,0 +1,9 @@
+package com.company.service;
+
+public class App5 {
+    public static void main(String[] args) {
+        // 1. Export currency list to EXCEL
+        // 2. Export currency list to PDF
+        // 3. Export currency list to WORD
+    }
+}
